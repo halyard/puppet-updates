7522ffec7b1f507c55fd552efc085397fa67debc
Les Aker <me@lesaker.org>
2022-03-11 00:14:15 -0500

    fix linting

--- manifests/archlinux.pp
+++ manifests/archlinux.pp
@@ -8,13 +8,13 @@ class updates::archlinux (
   file { '/etc/systemd/system/automatic-upgrades.service':
     ensure  => file,
     content => template('pacman/automatic-upgrades.service.erb'),
-    notify  => Service['automatic-upgrades.timer']
+    notify  => Service['automatic-upgrades.timer'],
   }
 
   file { '/etc/systemd/system/automatic-upgrades.timer':
     ensure  => file,
     content => template('pacman/automatic-upgrades.timer.erb'),
-    notify  => Service['automatic-upgrades.timer']
+    notify  => Service['automatic-upgrades.timer'],
   }
 
   service { 'automatic-upgrades.timer':
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 # @summary Enable automatic upgrades for the system
 #
+# @param bootdelay sets how long to wait before first run
+# @param frequency sets how often to run updates
 class updates (
   String $bootdelay = '1min',
   String $frequency = '86400'
